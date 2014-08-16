f8e5e4a9dacdd3e44cefdb93a92763bbf2f73c2c
TheBizzle <jason.bertsche@gmail.com>
2014-08-16 18:18:16 -0500

    Providing some basic A* star

---
+++ src/AStar/AStarLike.hs
@@ -0,0 +1,19 @@
+module AStar.AStarLike(getFreshLoc, maxItersBy) where
+
+  import Control.Arrow
+
+  import Data.Heap as Heap
+
+  import PathFindingCore.PathingMap.Coordinate
+
+  a |> f = f a
+
+  type CoordQueue = Heap MinPolicy PriorityCoordinate
+
+  maxItersBy :: Int -> Int -> Double -> Int
+  maxItersBy rows cols branchingFactor = rows |> ((*cols) >>> fromIntegral >>> (*branchingFactor) >>> floor)
+
+  getFreshLoc :: (Coordinate -> Bool) -> CoordQueue -> Maybe (PriorityCoordinate, CoordQueue)
+  getFreshLoc queue checkIsFamiliar = view trimmedQueue
+    where
+      trimmedQueue = Heap.dropWhile (coord >>> checkIsFamiliar) queue
