55c465902a4121b3d50ced3a6cf03fae522a7fb6
TheBizzle <jason.bertsche@gmail.com>
2014-09-22 20:55:14 -0500

    Shuffling around A* data types

--- src/AStar/AStarLike.hs
+++ src/AStar/AStarLike.hs
@@ -4,21 +4,21 @@ module AStar.AStarLike(runAStar) where
   import Control.Monad.State
 
   import Data.Array.IArray
-  import Data.Traversable as Traversable
-  import Data.Heap as Heap
+  import Data.Heap        as Heap
   import Data.Maybe
-  import Data.Set as Set
+  import Data.Set         as Set
+  import Data.Traversable as Traversable
 
   import PathFindingCore.PathingMap
-  import PathFindingCore.Status
   import PathFindingCore.PathingMap.Coordinate
   import PathFindingCore.PathingMap.Interpreter
+  import PathFindingCore.Status
 
+  import AStar.AStarData
   import AStar.Heuristic
 
   a |> f = f a
 
-  type CoordQueue = Heap MinPolicy (PriorityBundle LocationData)
   type AStarState = State AStarStepData
 
   runAStar :: PathingMapString -> (RunResult, AStarStepData)
@@ -94,42 +94,3 @@ module AStar.AStarLike(runAStar) where
       startData     = GridSD (Source start) 0
       gridSD        = (fmap (\_ -> Nothing) grid) // [(start, Just startData)]
       immData       = ImmSD heuristicFunc maxIters grid goal
-
-  data AStarStepData
-    = SD {
-      unSD      :: ImmutableStepData,
-      gridSDArr :: Array Coordinate (Maybe GridStepData),
-      locPair   :: LocationData,
-      queue     :: CoordQueue,
-      iters     :: Int,
-      visiteds  :: Set Coordinate -- We could do away with this and some of the cost-checking logic, I think, if we generalize this for only unidirectional A* with all edges having the same weight --JAB (8/23/14)
-    }
-
-  data ImmutableStepData
-    = ImmSD {
-      hValueOf :: Coordinate -> Double,
-      maxIters :: Int,
-      grid     :: PathingGrid,
-      goal     :: Coordinate
-    }
-
-  data GridStepData
-    = GridSD {
-      breadcrumb :: Breadcrumb,
-      cost       :: Double
-    } deriving (Eq, Show)
-
-  data LocationData
-    = Loc {
-      lcoord :: Coordinate,
-      lsd    :: GridStepData
-    } deriving (Eq, Show)
-
-  data PriorityBundle a
-    = PBundle {
-      priority :: Double,
-      item     :: a
-    } deriving (Eq, Show)
-
-  instance Eq x => Ord (PriorityBundle x) where
-    compare (PBundle p1 _) (PBundle p2 _) = compare p1 p2
